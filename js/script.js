67e1871a49184a9e470e69e985921703778b3796
RodicaNegura <RodicaElenaNegura@gmail.com>
2022-11-18 12:55:04 +0000

    adding more elements

--- js/script.js
+++ js/script.js
@@ -6,6 +6,14 @@ menu.onclick = () => {
   navbar.classList.toggle('active')
 }
 
+document.querySelector('#login-btn').onclick = () => {
+  document.querySelector('.login-form-container').classList.toggle('active')
+}
+
+document.querySelector('#close-login-form').onclick = () => {
+  document.querySelector('.login-form-container').classList.remove('active')
+}
+
 window.onscroll = () => {
   if(window.scrollY > 0) {
     document.querySelector('.header').classList.add('active')
@@ -23,4 +31,20 @@ window.onload = () => {
   }else{
     document.querySelector('.header').classList.add('active')
   }
+}
+
+document.querySelector('.home').onmousemove = (e) => {
+  document.querySelectorAll('.home-parallax').forEach(elm =>{
+    let speed = elm.getAttribute('data-speed')
+    let x = (window.innerWidth - e.pageX * speed) / 90
+    let y = (window.innerHeight - e.pageY * speed) / 90
+
+    elm.style.transform = `translateX(${y}px) translateY(${x}px)`
+  })
+}
+
+document.querySelector('.home').onmouseleave = () => {
+  document.querySelectorAll('.home-parallax').forEach(elm =>{
+    elm.style.transform = `translateX(0px) translateY(0px)`
+  })
 }
