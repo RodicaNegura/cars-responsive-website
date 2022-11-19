efd72fc017a9b1a1709f34317089e07c114d8796
RodicaNegura <RodicaElenaNegura@gmail.com>
2022-11-19 16:13:30 +0000

    reviews section

--- js/script.js
+++ js/script.js
@@ -97,7 +97,34 @@ var swiper = new Swiper(".featured-slider", {
     768: {
       slidesPerView: 2,
     },
-    1040: {
+    991: {
+      slidesPerView: 3,
+    },
+  },
+})
+
+var swiper = new Swiper(".reviews-slider", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  loop:true,
+  grabCursor:true,
+  centeredSlides:true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    991: {
       slidesPerView: 3,
     },
   },
